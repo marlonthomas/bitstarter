669f2238df1486822d922f3898d84e56f5bb283e
marlonthomas <marlon.thomas@hotmail.co.uk>
2013-07-14 23:36:49 +0000

    web.js modified to read text from index.html

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express'), fs = require("fs");
 
 var app = express.createServer(express.logger());
 
-var filename = "./index.html");
+var filename = "./index.html";
 
 var content = fs.readFileSync(filename);
 
 app.get('/', function(request, response) {
-  response.send(content.toString('utf-8')));
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
